0003d4b135d563ec8874952f096e7b49775c7260
gabriela thiesen <gabriela_thiesen@estudante.sc.senai.br>
2019-08-29 15:20:26 -0300

    criei a formula para a conversão dos valores de farenheint para celcius

--- main.js
+++ main.js
@@ -3,5 +3,9 @@ function ConvertTemp(direction){
     var Cobj = document.convert.TempCe;
     var Kobj = document.convert.TempKa;
 
-    
+    if (direction== "FtoC"){
+        Cobj.value = Math.round((Fobj.value-32)*(5/9));
+        Kobj.value = Math.round((parseInt(Cobj.value)+459.67)*(5/9));
+    }d
+
 }
